1fdf23f8aebb037b23cec555fc5dc8cf107b3729
Mohamed Sehsah <mo.sehsah@gmail.com>
2024-05-10 14:00:30 +0400

    init

---
+++ ai-proxy/src/main/java/com/vmware/scg/extensions/filter/AddMyCustomHeaderGatewayFilterFactory.java
@@ -0,0 +1,31 @@
+package com.vmware.scg.extensions.filter;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+@Component
+class AddMyCustomHeaderGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AddMyCustomHeaderGatewayFilterFactory.class);
+    private static final String MY_HEADER_KEY = "X-My-Header";
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            ServerWebExchange updatedExchange = exchange.mutate()
+                    .request(request -> request.headers(headers -> {
+                        headers.put(MY_HEADER_KEY, List.of("my-header-value"));
+                        LOGGER.info("Processed request, added " + MY_HEADER_KEY + " header");
+                    }))
+                    .build();
+            return chain.filter(updatedExchange);
+        };
+    }
+}
